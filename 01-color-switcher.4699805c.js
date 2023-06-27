7a4b882be56ab3bf80b904c5442d359b9e97f50a
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-06-27 18:23:31 +0000

    Deploying to gh-pages from @ VladaHrazhdiian/goit-js-hw-09@2a03957c8aac3d672d2c7569b883a44da5477234 🚀

---
+++ 01-color-switcher.4699805c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){e.removeAttribute("disabled"),a=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.background=t}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(a),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.4699805c.js.map
